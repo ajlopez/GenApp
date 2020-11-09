9ddd6b5dfd187aa94d32b98673cef669bd202625
ajlopez <webmaster@ajlopez.com>
2020-11-09 14:39:36 -0300

    Generate title

--- test/generate.js
+++ test/generate.js
@@ -7,6 +7,7 @@ const templatesFolder = path.join(__dirname, 'templates');
 
 exports['generate text'] = function (test) {
     const model = {
+        type: 'text',
         text: 'Hello, world'
     };
     
@@ -14,5 +15,17 @@ exports['generate text'] = function (test) {
     
     test.ok(result);
     test.equal(result, '<p>\r\nHello, world\r\n</p>');
+};
+
+exports['generate title'] = function (test) {
+    const model = {
+        type: 'title',
+        text: 'Hello, world'
+    };
+    
+    const result = generate(model, templatesFolder).trim();
+    
+    test.ok(result);
+    test.equal(result, '<h1>Hello, world</h1>');
 };
 
